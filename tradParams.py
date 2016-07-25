ac2c79963d27839b386dab0105df64aa9b1bc0e8
Elea <elea.greugny@insa-lyon.fr>
2016-07-25 17:09:52 +0200

    ajout des dictionnaires fixes aux paramètres

--- tradParams.py
+++ tradParams.py
@@ -19,3 +19,29 @@ class tradParams :
 
     #dimension de la box d'un opérateur logique
     LOG_OP_DIM = 42
+
+    #dictionnaire des types de glyphes
+    DIC_GLYPH_TYPE = {'ba' : GlyphClass.BIOLOGICAL_ACTIVITY,
+    'perturbation' : GlyphClass.PERTURBATION,
+    'phenotype' : GlyphClass.PHENOTYPE, 'and' : GlyphClass.AND,
+    'or' : GlyphClass.OR, 'not' : GlyphClass.NOT,
+    'delay' : GlyphClass.DELAY, 'compartment' : GlyphClass.COMPARTMENT,
+    'biologicalActivity' : GlyphClass.BIOLOGICAL_ACTIVITY}
+
+    #dictionnaire de type d'arcs
+    DIC_ARC_TYPE = {'input' : ArcClass.LOGIC_ARC,
+    'stimulates' : ArcClass.POSITIVE_INFLUENCE,
+    'inhibits' : ArcClass.NEGATIVE_INFLUENCE,
+    'unknownInfluences' : ArcClass.UNKNOWN_INFLUENCE,
+    'necessarilyStimulates' : ArcClass.NECESSARY_STIMULATION}
+
+    #dictionnaire de type d'unité d'information
+    DIC_UI_TYPE = {'macromolecule' : 'macromolecule',
+    'naf' : 'nucleic acid feature', 'complex' : 'complex',
+    'simplechemical' : 'simple chemical',
+    'unspecifiedentity' : 'unspecified entity', 'void' : '"void"'}
+
+    #dictionnaire des opérateurs logiques et labels associés
+    DIC_LOG_OP = {GlyphClass.AND : 'AND', GlyphClass.OR : 'OR',
+    GlyphClass.NOT : 'NOT', GlyphClass.DELAY : 'T'}
+
